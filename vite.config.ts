47b31e406af2b4f0278233959b69ed9c58bb09fa
masashiono0611 <masashi.ono.0611@gmail.com>
2025-03-10 02:20:37 +0800

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/my-ton-sc-part3/',
+  base: '/my-ton-sc-part3-2/',
 });
